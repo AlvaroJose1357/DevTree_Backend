3b72445b290825fb80a7455d2b19c1af4d5acea4
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-04 23:58:41 -0500

    Add User model with schema definition for name, email, and password

---
+++ src/models/User.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  // el trim lo que hace es que si existen espacios en blanco al principio o al final de la cadena, los elimina
+  name: { type: String, required: true, trim: true },
+  // el unique lo que hace es que no se pueden repetir los elementos en la base de datos
+  email: { type: String, required: true, trim: true, unique: true },
+  password: { type: String, required: true, trim: true },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
