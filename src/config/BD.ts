ce54c491fddfa177af104d90026eda476b18f78c
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-04 21:04:29 -0500

    Add colors dependency and enhance console logging for database and server connections

--- src/config/BD.ts
+++ src/config/BD.ts
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
+import colors from "colors";
 import { MONGODB_URL } from "./Process";
 
 export const connectBD = async () => {
   try {
     const { connection } = await mongoose.connect(MONGODB_URL);
     const urlBase = `${connection.host}:${connection.port}/${connection.name}`;
-    console.log("Database connected on", urlBase);
+    console.log(colors.cyan.bold(`Database connected on ${urlBase}`));
   } catch (error) {
-    console.error("Error connecting to database");
-    console.error(error);
+    console.error(
+      colors.bgRed.white.bold(`Error connecting to the database ${error}`)
+    );
     process.exit(1);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import colors from "colors";
 import app from "./server";
 import { PORT } from "./config/Process";
 
 const Port = PORT;
 
 app.listen(Port, () => {
-  console.log("Server listening on port " + Port);
+  console.log(colors.blue.bold(`Server running on port ${Port}`));
 });
