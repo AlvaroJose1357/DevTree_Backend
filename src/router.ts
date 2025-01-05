edb30b505e01ffa3f27e63dff428a18037d759d1
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-05 01:04:12 -0500

    feat(auth): Implement user registration endpoint with database integration

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import User from "./models/User";
 const router = Router();
 
 // Autenticacion
-router.post("/auth/register", (req, res) => {
-  res.send("Login");
+router.post("/auth/register", async (req, res) => {
+  const newUser = new User(req.body);
+  await newUser.save();
+  res.status(201).json(newUser);
 });
 
 router.post("/auth/login", (req, res) => {
