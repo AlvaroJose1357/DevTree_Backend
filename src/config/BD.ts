650192b0cfc346ec28b373fd66a63f5b5d366624
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-08 19:20:00 -0500

    feat(db): Add error handling for undefined MONGODB_URL in database connection

--- src/config/BD.ts
+++ src/config/BD.ts
@@ -4,6 +4,9 @@ import { MONGODB_URL } from "./Process";
 
 export const connectBD = async () => {
   try {
+    if (!MONGODB_URL) {
+      throw new Error("MONGODB_URL is not defined");
+    }
     const { connection } = await mongoose.connect(MONGODB_URL);
     const urlBase = `${connection.host}:${connection.port}/${connection.name}`;
     console.log(colors.cyan.bold(`Database connected on ${urlBase}`));
