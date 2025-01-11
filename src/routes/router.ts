ef65e0507d1c4d5a26e7a4a083a3c9c17ffd13e0
Alvaro Jose Ibarra <joeibarrafernandez@gmail.com>
2025-01-10 21:20:44 -0500

    feat(auth): Add validation to user registration endpoint using express-validator

---
+++ src/routes/router.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { body } from "express-validator";
+import { createAccount } from "../controllers/users";
+const router = Router();
+
+// Autenticacion
+router.post(
+  "/auth/register",
+  // validacion con express-validator
+  body("handle").notEmpty().withMessage("Handle is required"),
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").isEmail().withMessage("E-mail not valid"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
+  createAccount
+);
+
+router.post("/auth/login", (req, res) => {
+  res.send("Login");
+});
+
+export default router;
